2049ef19dd3eb39c0062e2e8cd2482085362b779
Amy R. <ellis.a0426@yahoo.com>
2025-11-25 17:14:50 -0500

    added starter files
    -the beginning
    -created ReadingStatus enumeration with 4 status constants

---
+++ ProjectFinalLibraryMgmtSystem/src/ReadingStatus.java
@@ -0,0 +1,10 @@
+/*
+@projectdeatils Name: Amy Rogers | Course: Programming | personal library management
+ */
+
+    public enum ReadingStatus {
+        NOT_READ //book has not been started
+        CURRENTLY_READING //book is actively being read
+        READ_//book has been completed
+        DNF//'Did Not FInish' book was started but abandoned
+        }
